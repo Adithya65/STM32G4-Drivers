5c4577f541e03b5f552140aea3cb3b74ba1075e0
adithyasm65 <adithyasm6@gmail.com>
2024-09-04 21:52:19 +0530

    Uart:remove unnecessary files

--- UART/uart.h
+++ UART/uart.h
@@ -1,29 +1,29 @@
 #ifndef __STM_UART__
 #define __STM_UART__
 
-#define REG_WR(ADDRESS, VALUE)  (*(uint32_t*)(ADDRESS) = (VALUE))
-#define REG_RD(ADDRESS)         (*(uint32_t*)(ADDRESS))
+#define REG_WR(ADDRESS, VALUE)	(*(uint32_t*)(ADDRESS) = (VALUE))
+#define REG_RD(ADDRESS)			(*(uint32_t*)(ADDRESS))
 
-#define USART1_BASE_ADD                 ( 0x40004400 )
+#define USART1_BASE_ADD					( 0x40004400 )
 #define GPIOC_BASE_ADD					( 0x48000800 )
 #define RCC_BASE_ADD					( 0x40021000 )
 
-#define RCC_APHB2ENR_OFFSET 			( 0x4C )
+#define RCC_APHB2ENR_OFFSET				( 0x4C )
 #define RCC_APB2ENR_OFFSET				( 0x60 )
 
 #define GPIOC_MODE_REG_OFFSET			( 0x0 )
-#define GPIOC_AFRL_REG_OFFSET           ( 0x20 ) 
+#define GPIOC_AFRL_REG_OFFSET			( 0x20 ) 
 
-#define USART1_CR1_REG_OFFSET           ( 0x00 )
+#define USART1_CR1_REG_OFFSET			( 0x00 )
 #define USART1_CR2_REG_OFFSET			( 0x04 )
 #define USART1_CR3_REG_OFFSET			( 0x08 )
 #define USART1_BRR_REG_OFFSET			( 0x0C )
 #define USART1_RDR_REG_OFFSET			( 0x24 )
 #define USART1_TDR_REG_OFFSET			( 0x28 )
 
-#define USART1_CR1_REG_ADD              ( USART1_BASE_ADD + USART1_CR1_REG_OFFSET )
-#define USART1_CR2_REG_ADD              ( USART1_BASE_ADD + USART1_CR2_REG_OFFSET )
-#define USART1_CR3_REG_ADD              ( USART1_BASE_ADD + USART1_CR3_REG_OFFSET )
+#define USART1_CR1_REG_ADD				( USART1_BASE_ADD + USART1_CR1_REG_OFFSET )
+#define USART1_CR2_REG_ADD				( USART1_BASE_ADD + USART1_CR2_REG_OFFSET )
+#define USART1_CR3_REG_ADD				( USART1_BASE_ADD + USART1_CR3_REG_OFFSET )
 #define USART1_BRR_REG_ADD				( USART1_BASE_ADD + USART1_BRR_REG_OFFSET )
 #define USART1_RDR_REG_ADD				( USART1_BASE_ADD + USART1_BRR_REG_OFFSET )
 #define USART1_TDR_REG_ADD				( USART1_BASE_ADD + USART1_TDR_REG_OFFSET )
@@ -31,7 +31,7 @@
 #define GPIOC_MODE_REG_ADD				( GPIOC_BASE_ADD + GPIOC_MODE_REG_OFFSET )
 #define GPIOC_AFRL_REG_ADD				( GPIOC_BASE_ADD + GPIOC_AFRL_REG_OFFSET )
 
-#define RCC_AHB2ENR_ADD 				( RCC_BASE_ADD +  RCC_AHB2ENR_OFFSET )
+#define RCC_AHB2ENR_ADD					( RCC_BASE_ADD +  RCC_AHB2ENR_OFFSET )
 #define RCC_APB2ENR_ADD					( RCC_BASE_ADD +  RCC_APB2ENR_OFFSET )
  
 #define EN_USART1_CLK					( 0x1 << 14 )
@@ -51,7 +51,7 @@
 #define EN_RX							( 0x1 << 2 )
 #define EN_TX							( 0x1 << 3 )
 #define SET_WORD_LEN_M0				   ~(( 0x1 << 12 )| ( 0x1 << 28 ))
-#define EN_PARITY_CTRL				    ( 1 << 10 )
+#define EN_PARITY_CTRL					( 1 << 10 )
 #define SET_EVEN_PARITY				   ~( 1 << 9 )
 #define SET_STOP_BITS0				   ~(( 0x1 << 12 )| ( 0x1 << 13 ))
 
